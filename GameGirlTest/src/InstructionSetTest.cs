d62a479dd2f45008997c2e3726aba7e4a2e68f75
Étienne Plante <zerox029@gmail.com>
2022-08-18 19:46:53 -0400

    Added load instructions

--- GameGirlTest/src/InstructionSetTest.cs
+++ GameGirlTest/src/InstructionSetTest.cs
@@ -12,11 +12,11 @@ namespace GameGirlTest
     private InstructionSet instructionSet;
 
     [TestMethod]
-    public void TestCPL()
+    public void TestInstruction_0x2F()
     {
       //GIVEN
       registers = new Registers();
-      registers.A = 0b00000000;
+      registers.A = 0b00001111;
 
       mmu = new MMU();
 
@@ -26,7 +26,170 @@ namespace GameGirlTest
       instructionSet.RunInstruction(0x2F);
 
       //THEN
-      Assert.AreEqual(0b11111111, registers.A);
+      Assert.AreEqual(0b11110000, registers.A);
+    }
+
+    [TestMethod]
+    public void TestInstruction_0x3F()
+    {
+      //GIVEN
+      registers = new Registers();
+      registers.SetFlag(Flag.CARRY);
+
+      mmu = new MMU();
+
+      instructionSet = new InstructionSet(registers, mmu);
+
+      //WHEN
+      instructionSet.RunInstruction(0x3F);
+
+      //THEN
+      Assert.IsFalse(registers.GetFlag(Flag.CARRY));
+    }
+
+    [TestMethod]
+    public void TestInstruction_0x40()
+    {
+      //GIVEN
+      registers = new Registers();
+      registers.B = 20;
+
+      mmu = new MMU();
+
+      instructionSet = new InstructionSet(registers, mmu);
+
+      //WHEN
+      instructionSet.RunInstruction(0x40);
+
+      //THEN
+      Assert.AreEqual(registers.B, 20);
+    }
+
+    [TestMethod]
+    public void TestInstruction_0x41()
+    {
+      //GIVEN
+      registers = new Registers();
+      registers.C = 20;
+
+      mmu = new MMU();
+
+      instructionSet = new InstructionSet(registers, mmu);
+
+      //WHEN
+      instructionSet.RunInstruction(0x41);
+
+      //THEN
+      Assert.AreEqual(registers.B, 20);
+    }
+
+    [TestMethod]
+    public void TestInstruction_0x42()
+    {
+      //GIVEN
+      registers = new Registers();
+      registers.D = 20;
+
+      mmu = new MMU();
+
+      instructionSet = new InstructionSet(registers, mmu);
+
+      //WHEN
+      instructionSet.RunInstruction(0x42);
+
+      //THEN
+      Assert.AreEqual(registers.B, 20);
+    }
+
+    [TestMethod]
+    public void TestInstruction_0x43()
+    {
+      //GIVEN
+      registers = new Registers();
+      registers.E = 20;
+
+      mmu = new MMU();
+
+      instructionSet = new InstructionSet(registers, mmu);
+
+      //WHEN
+      instructionSet.RunInstruction(0x43);
+
+      //THEN
+      Assert.AreEqual(registers.B, 20);
+    }
+
+    [TestMethod]
+    public void TestInstruction_0x44()
+    {
+      //GIVEN
+      registers = new Registers();
+      registers.H = 20;
+
+      mmu = new MMU();
+
+      instructionSet = new InstructionSet(registers, mmu);
+
+      //WHEN
+      instructionSet.RunInstruction(0x44);
+
+      //THEN
+      Assert.AreEqual(registers.B, 20);
+    }
+
+    [TestMethod]
+    public void TestInstruction_0x45()
+    {
+      //GIVEN
+      registers = new Registers();
+      registers.L = 20;
+
+      mmu = new MMU();
+
+      instructionSet = new InstructionSet(registers, mmu);
+
+      //WHEN
+      instructionSet.RunInstruction(0x45);
+
+      //THEN
+      Assert.AreEqual(registers.B, 20);
+    }
+
+    [TestMethod]
+    public void TestInstruction_0x46()
+    {
+      //GIVEN
+      registers = new Registers();
+      registers.HL = 0x2F78;
+
+      mmu = new MMU();
+      mmu.WriteByte(0x2F78, 20);
+
+      instructionSet = new InstructionSet(registers, mmu);
+
+      //WHEN
+      instructionSet.RunInstruction(0x46);
+
+      //THEN
+      Assert.AreEqual(registers.B, 20);
+    }
+
+    [TestMethod]
+    public void TestInstruction_0x47()
+    {
+      //GIVEN
+      registers = new Registers();
+      registers.A = 20;
+
+      mmu = new MMU();
+
+      instructionSet = new InstructionSet(registers, mmu);
+
+      //WHEN
+      instructionSet.RunInstruction(0x47);
+
+      //THEN
+      Assert.AreEqual(registers.B, 20);
     }
   }
 }
